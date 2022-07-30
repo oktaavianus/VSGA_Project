475d19922b268fd7d414f2b64954d6eb8e44c3dd
oktaavianus <oktaavianus.kurniadi@gmail.com>
2022-07-31 00:38:01 +0700

    Registrasi Selesai, Login Selesai, Password Terbcyrpt

--- tugasP7/loginView.php
+++ tugasP7/loginView.php
@@ -20,15 +20,23 @@
           </h6>
         </div>
         <form method="POST" action="commandPHP/loginProcess.php">
-          <div class="mb-4">
-            <label for="exampleInputEmail1" class="form-label">Username*</label>
-            <input type="text" class="form-control" name="username" placeholder="Input your username here" required />
+          <!-- Username Input -->
+          <label for="username" class="form-label">Username</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg></span>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Input your username here" required />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password*</label>
-            <input type="password" class="form-control" name="password" placeholder="Input your password" required />
+          <!-- Password Input -->
+          <label for="password" class="form-label">Password</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-unlock-fill" viewBox="0 0 16 16">
+                <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z" />
+              </svg></span>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Input your password" required />
           </div>
-          <div class="d-flex justify-content-between">
+          <!-- <div class="d-flex justify-content-between">
             <div class="mb-3 form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1" />
               <label class="form-check-label" for="exampleCheck1">Remember me</label>
@@ -36,9 +44,9 @@
             <div>
               <a href="#" class="forgotpassword">Forgot Password?</a>
             </div>
-          </div>
-          <div class="d-grid mt-3">
-            <button type="submit" class="btn btn-success btn-login">
+          </div> -->
+          <div class="d-grid mt-5">
+            <button type="submit" class="btn btn-success btn-login" name="Login">
               Login
             </button>
           </div>
--- tugasP7/registerView.php
+++ tugasP7/registerView.php
@@ -19,27 +19,37 @@
             Please register here
           </h6>
         </div>
-        <form>
+        <form method="POST" action="commandPHP/registerProcess.php">
           <!-- Email -->
-          <div class="mb-4">
-            <label for="email" class="form-label">Email*</label>
-            <input type="email" class="form-control" id="email" placeholder="Input your email here" />
+          <label for="email" class="form-label">Email</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase-fill" viewBox="0 0 16 16">
+                <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
+                <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
+              </svg></span>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Input your email here" />
           </div>
           <!-- /Email -->
           <!-- Username -->
-          <div class="mb-4">
-            <label for="Username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="Username" placeholder="Your username" />
+          <label for="username" class="form-label">Username</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg></span>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Input your username here" required />
           </div>
           <!-- /Username -->
           <!-- Password -->
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password*</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Input your password" />
+          <label for="password" class="form-label">Password</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-unlock-fill" viewBox="0 0 16 16">
+                <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z" />
+              </svg></span>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Input your password" required />
           </div>
           <!-- /Password -->
           <div class="d-grid mt-3">
-            <button type="submit" class="btn btn-success btn-login">
+            <button type="submit" class="btn btn-success btn-login" name="Register">
               Register
             </button>
           </div>
@@ -54,6 +64,8 @@
 
   <!-- SCRIPT -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <!-- SCRIPT PHP -->
+
 </body>
 
 </html>
